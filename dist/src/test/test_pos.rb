a18180c82334e15ce8a757c720d632452eee09ae
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-14 16:05:31 +0900

    test_pos: shouldの位置がずれたのを修正

--- dist/src/test/test_pos.rb
+++ dist/src/test/test_pos.rb
@@ -1,33 +1,35 @@
 # coding: utf-8
 
-class TestPos < Test::Unit::TestCase
-  should ".x, .yでアクセスできること" do
-    pos = LambdaLifter::Pos.new(1, 1)
-    assert_equal 1, pos.x
-    assert_equal 1, pos.y
-  end
+class LambdaLifter
+  class TestPos < Test::Unit::TestCase
+    should ".x, .yでアクセスできること" do
+      pos = Pos.new(1, 1)
+      assert_equal 1, pos.x
+      assert_equal 1, pos.y
+    end
 
-  should "inspectは(x, y)の文字列を返すこと" do
-    assert_equal "(1, 2)", LambdaLifter::Pos.new(1, 2).inspect
-  end
+    should "inspectは(x, y)の文字列を返すこと" do
+      assert_equal "(1, 2)", LambdaLifter::Pos.new(1, 2).inspect
+    end
 
-  should "x, yを基にした比較が可能であること" do
-    a = LambdaLifter::Pos.new(1, 1)
-    b = LambdaLifter::Pos.new(1, 1)
-    c = LambdaLifter::Pos.new(1, 2)
-    assert_equal a, b
-    assert a == b
-    assert a != c
-  end
+    should "x, yを基にした比較が可能であること" do
+      a = LambdaLifter::Pos.new(1, 1)
+      b = LambdaLifter::Pos.new(1, 1)
+      c = LambdaLifter::Pos.new(1, 2)
+      assert_equal a, b
+      assert a == b
+      assert a != c
+    end
 
-  should "hashのkeyとして指定できること" do
-    h = {}
-    a = LambdaLifter::Pos.new(1, 2)
-    b =  LambdaLifter::Pos.new(1, 2)
-    h[a] = true
-    h[b] = true
-    assert a.eql?(b)
-    assert_equal a.hash, b.hash
-    assert_equal 1, h.keys.size
+    should "hashのkeyとして指定できること" do
+      h = {}
+      a = LambdaLifter::Pos.new(1, 2)
+      b =  LambdaLifter::Pos.new(1, 2)
+      h[a] = true
+      h[b] = true
+      assert a.eql?(b)
+      assert_equal a.hash, b.hash
+      assert_equal 1, h.keys.size
+    end
   end
 end
