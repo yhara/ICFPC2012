637d5aa222523c27fb8fa4830e2fd0f0e6044218
Yuki Morohoshi <morohoshi@netlab.jp>
2012-07-14 22:25:40 +0900

    Mineクラスのテストを追加。

--- dist/src/test/test_mine.rb
+++ dist/src/test/test_mine.rb
@@ -2,6 +2,15 @@
 
 class LambdaLifter
   class TestMine < Test::Unit::TestCase
+
+    ROBOT_CENTERED_MAP = <<-'EOD'
+#####
+#   #
+# R #
+#   #
+#####
+        EOD
+
     setup do
       @mine = Mine.new(<<-'EOD')
 #######
@@ -29,23 +38,50 @@ class LambdaLifter
     end
 
     should "mine[x, y]でその座標にあるものを返すこと" do
-      pend
       assert_equal :wall, @mine[1, 1] 
       assert_equal :robot, @mine[2, 2] 
       assert_equal :lambda, @mine[3, 2] 
     end
 
     context "step!が呼ばれたとき" do
-      should "ロボットを動かすこと" do
-        pend
-        mine = Mine.new(<<-'EOD')
-####
-#R #
-####
-        EOD
+      should "コマンドRでロボットを右に動かすこと" do
+        mine = Mine.new(ROBOT_CENTERED_MAP)
         mine.step!("R")
+        assert_equal [:wall, :empty, :empty, :robot, :wall], mine.raw_map[2]
+      end
 
-        assert_equal [:wall, :empty, :robot, :wall], mine.raw_map[1]
+      should "コマンドLでロボットを左に動かすこと" do
+        mine = Mine.new(ROBOT_CENTERED_MAP)
+        mine.step!("L")
+        assert_equal [:wall, :robot, :empty, :empty, :wall], mine.raw_map[2]
+      end
+
+      should "コマンドUでロボットを上に動かすこと" do
+        mine = Mine.new(ROBOT_CENTERED_MAP)
+        mine.step!("U")
+        assert_equal [:wall, :empty, :robot, :empty, :wall], mine.raw_map[1]
+      end
+
+      should "コマンドDでロボットを下に動かすこと" do
+        mine = Mine.new(ROBOT_CENTERED_MAP)
+        mine.step!("D")
+        assert_equal [:wall, :empty, :robot, :empty, :wall], mine.raw_map[3]
+      end
+
+      should "コマンドWでロボットが動かないこと" do
+        pend
+        mine = Mine.new(ROBOT_CENTERED_MAP)
+        mine.step!("W")
+        assert_equal [:wall, :empty, :robot, :empty, :wall], mine.raw_map[2]
+      end
+
+      should "コマンドRLでロボットが元の位置に戻ること" do
+        pend
+        mine = Mine.new(ROBOT_CENTERED_MAP)
+        mine.step!("R")
+        assert_equal [:wall, :empty, :empty, :robot, :wall], mine.raw_map[2]
+        mine.step!("L")
+        assert_equal [:wall, :empty, :robot, :empty, :wall], mine.raw_map[2]
       end
     end
 
