4a50c7f827b5a71c7e05e4c9b0554609cd458136
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-14 21:59:27 +0900

    test_robot: contextでくくった

--- dist/src/test/test_robot.rb
+++ dist/src/test/test_robot.rb
@@ -8,31 +8,32 @@ class LambdaLifter
       assert_equal 1, robot.y
     end
 
-    should "移動先が空、地面、Lambda、Open Lambda Liftであれば移動可能であること" do
-      mine = Mine.new(<<-'EOD')
+    context "mobable?が呼ばれたとき" do
+      should "移動先が空、地面、Lambda、Open Lambda Liftであれば移動可能であること" do
+        mine = Mine.new(<<-'EOD')
 #####
 # \ #
 # R.#
 # O #
 #####
-      EOD
-      assert_equal true, mine.robot.movable?(:left), "空"
-      assert_equal true, mine.robot.movable?(:right), "地面"
-      assert_equal true, mine.robot.movable?(:up), "Lambda"
-      assert_equal true, mine.robot.movable?(:down), "Open Lambda Lift"
-    end
+        EOD
+        assert_equal true, mine.robot.movable?(:left), "空"
+        assert_equal true, mine.robot.movable?(:right), "地面"
+        assert_equal true, mine.robot.movable?(:up), "Lambda"
+        assert_equal true, mine.robot.movable?(:down), "Open Lambda Lift"
+      end
 
-    should "移動先が岩でもその先が空白なら移動可能であること" do
-      mine = Mine.new(<<-'EOD')
+      should "移動先が岩でもその先が空白なら移動可能であること" do
+        mine = Mine.new(<<-'EOD')
 #####
 #R* #
 #####
-      EOD
-      assert_equal true, mine.robot.movable?(:right)
-    end
+        EOD
+        assert_equal true, mine.robot.movable?(:right)
+      end
 
-    should "移動先が岩でその先が空白じゃなければ移動不可であること" do
-      mine = Mine.new(<<-'EOD')
+      should "移動先が岩でその先が空白じゃなければ移動不可であること" do
+        mine = Mine.new(<<-'EOD')
 #######
 #  \  #
 #  *  #
@@ -40,25 +41,25 @@ class LambdaLifter
 #  *  #
 #  L  #
 #######
-      EOD
-      assert_equal false, mine.robot.movable?(:left), "壁"
-      assert_equal false, mine.robot.movable?(:right), "岩"
-      assert_equal false, mine.robot.movable?(:up), "Lambda"
-      assert_equal false, mine.robot.movable?(:down), "Closed Lambda Lift"
-    end
+        EOD
+        assert_equal false, mine.robot.movable?(:left), "壁"
+        assert_equal false, mine.robot.movable?(:right), "岩"
+        assert_equal false, mine.robot.movable?(:up), "Lambda"
+        assert_equal false, mine.robot.movable?(:down), "Closed Lambda Lift"
+      end
 
-    should "それ以外（壁かClosed Lambda Lift）なら移動不可であること" do
-      mine = Mine.new(<<-'EOD')
+      should "それ以外（壁かClosed Lambda Lift）なら移動不可であること" do
+        mine = Mine.new(<<-'EOD')
 ####
 #RL#
 ####
-      EOD
-      assert_equal false, mine.robot.movable?(:left), "壁"
-      assert_equal false, mine.robot.movable?(:right), "Closed Lambda Lift"
-    end
+        EOD
+        assert_equal false, mine.robot.movable?(:left), "壁"
+        assert_equal false, mine.robot.movable?(:right), "Closed Lambda Lift"
+      end
 
-    should "移動可能なPosの配列を返せること" do
-      mine = Mine.new(<<-'EOD')
+      should "移動可能なPosの配列を返せること" do
+        mine = Mine.new(<<-'EOD')
 #######
 #     #
 #  #  #
@@ -66,8 +67,9 @@ class LambdaLifter
 #  .  #
 #     #
 #######
-      EOD
-      assert_equal [Pos.new(5, 4), Pos.new(4, 3)], mine.robot.movable_positions
+        EOD
+        assert_equal [Pos.new(5, 4), Pos.new(4, 3)], mine.robot.movable_positions
+      end
     end
   end
 end
