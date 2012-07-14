f7209ea244022193571f47a3f75a7d50a37e7618
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-14 15:02:32 +0900

    tools/random_map: --floodを追加

--- tools/random_map.rb
+++ tools/random_map.rb
@@ -6,7 +6,9 @@ class Mine
   def initialize(wid, hei)
     @wid, @hei = wid, hei
     @mine = Array.new(hei){ Array.new(wid){ nil } }
+    @extra = ""
   end
+  attr_accessor :extra
 
   # 左下が1,1
   def [](x,y)
@@ -32,8 +34,8 @@ class Mine
         when String then item
         when nil then " "
         end
-      }.join
-    }.join("\n")
+      }.join + "\n"
+    }.join + @extra
   end
 end
 
@@ -43,6 +45,7 @@ opts = Trollop.options do
   opt :width, "mapの幅(デフォルト: 10)", default: 10
   opt :height, "mapの高さ(デフォルト: 10)", default: 10
   opt :type, "タイプ(easy/mid)", default: "easy"
+  opt :flood, "洪水があるかどうか", default: false
   opt :help, "ヘルプ", short: :none
 end
 Trollop.die if opts[:help] 
@@ -51,9 +54,11 @@ wid = opts[:width]
 hei = opts[:height]
 
 mine = Mine.new(wid, hei)
+
 # 壁生成
 (1..wid).each{|x| mine[x, 1] = "#"; mine[x, hei] = "#"}
 (1..hei).each{|y| mine[1, y] = "#"; mine[wid, y] = "#"}
+
 # 中身生成
 objects = ("*"*10 + "\\"*10 + "."*70 + " "*30).chars.to_a
 (2..wid-1).each do |x|
@@ -61,9 +66,29 @@ objects = ("*"*10 + "\\"*10 + "."*70 + " "*30).chars.to_a
     mine[x, y] = objects.sample
   end
 end
+
+# 壁2
+if opts[:type] == "mid"
+  #obstacle = Array.new(@wid)
+end
+
 # ゴール生成
 mine[rand(wid-2)+2, 1] = "L"
+
 # スタート生成
 mine[rand(wid-2)+2, hei-1] = "R"
+
+# 洪水
+if opts[:flood]
+  water = rand(hei / 5)
+  flooding = rand(hei / 10) + 1
+  waterproof = rand(flooding*2) + (flooding*3)
+  mine.extra = [
+    "",
+    "Water #{water}",
+    "Flooding #{flooding}",
+    "Waterproof #{waterproof}",
+  ].join("\n")
+end
 
 puts mine.to_s
