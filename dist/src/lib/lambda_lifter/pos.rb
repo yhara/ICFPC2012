cb8fd99d51d92cbbc17dd72610093178f0bd01ab
Narihiro Nakamura <authornari@gmail.com>
2012-07-14 15:25:02 +0900

    posはimmutableに

--- dist/src/lib/lambda_lifter/pos.rb
+++ dist/src/lib/lambda_lifter/pos.rb
@@ -2,30 +2,10 @@ class LambdaLifter
   class Pos
     include Comparable
 
-    attr_accessor :x, :y
+    attr_reader :x, :y
     def initialize(x, y)
-      move_to(x, y)
-    end
-
-    def move_to(x, y)
       @x = x
       @y = y
-    end
-
-    def left
-      @x=-1
-    end
-
-    def right
-      @x=+1
-    end
-
-    def down
-      @y=-1
-    end
-
-    def up
-      @y=+1
     end
 
     def <=>(other)
