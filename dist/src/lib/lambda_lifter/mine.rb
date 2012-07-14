08970045733026c9d023fe40940e6f216728fc33
Akihiro Sada <yuya@j96.org>
2012-07-14 20:04:51 +0900

    Mineクラスのstep!メソッドを修正

--- dist/src/lib/lambda_lifter/mine.rb
+++ dist/src/lib/lambda_lifter/mine.rb
@@ -12,6 +12,17 @@ class LambdaLifter
       ' ' => :empty
     }
 
+    COMMANDS = {
+      'L' => :left,
+      'R' => :right,
+      'U' => :up,
+      'D' => :down,
+      'W' => :wait,
+      'A' => :abort
+    }
+
+    attr_accessor :robot, :lambdas, :updated_map
+
     def initialize(mine_description)
       @map = nil
       parse(mine_description)
@@ -41,7 +52,63 @@ class LambdaLifter
   
     # マップを書き換える。
     def step!(command)
-      # TODO
+      @updated_map = @map.dup
+      command = COMMAND[command]
+
+      if @robot.movable?(command)
+        set(@robot.x, @robot.y, :empty)
+        case command
+        when :left
+          set(@robot.x - 1, @robot.y, :robot)
+        when :right
+          set(@robot.x + 1, @robot.y, :robot)
+        when :up
+          set(@robot.x, @robot.y + 1, :robot)
+        when :down
+          set(@robot.x, @robot.y - 1, :robot)
+        end
+      end
+
+      # 岩を更新する
+      width = 1
+      height = 1
+      while @height <= height
+        while @width <= width
+          layout = self[width, height]
+          case layout
+          when :rock
+            if self[width, height - 1] == :empty
+              set(width, height, :empty)
+              set(width, height - 1, :rock)
+            elsif self[width, height - 1] == :rock &&
+                  self[width + 1, height] == :empty &&
+                  self[width + 1, height - 1] == :empty
+              set(width, height, :empty)
+              set(width + 1, height - 1, :rock)
+            elsif self[width, height - 1] == :rock &&
+                  self[width + 1, height] != :empty &&
+                  self[width - 1, height - 1] = :empty ||
+                  self[width, height - 1] == :rock &&
+                  self[width + 1, height - 1] != :empty &&
+                  self[width - 1, height - 1] = :empty
+              set(width, height, :empty)
+              set(width - 1, height - 1, :rock)
+            elsif self[width, height - 1] == :lambda &&
+                  self[width + 1, height] == :empty &&
+                  self[width + 1, height - 1] == :empty
+              set(width, height, :empty)
+              set(width + 1, height - 1, :rock)
+            end
+          when :closed_lift
+            if @lambdas == 0
+             set(width, height, :open_lift)
+            end
+          end
+        width += 1
+        end
+        width = 0
+        height += 1
+      end
     end
   
     def finished?
@@ -51,16 +118,32 @@ class LambdaLifter
 
     private
 
+    def get(x, y)
+      @updated_map[@updated_map.length - y][x - 1]
+    end
+
+    def set(x, y, layout)
+      @updated_map[@updated_map.length - y][x - 1] = layout
+    end
+
     def parse(mine_description)
       mine_description = mine_description.split("\n")
+      @lambdas = 0
       grid = mine_description.each_with_object([]) do |line, g|
-        g << line.each_char.map {|c| LAYOUTS[c] }
+        g << line.each_char.map do |c|
+          layout = LAYOUTS[c]
+          if layout == :lambda
+            @lambdas += 1
+          end
+          layout
+        end
       end
-      longest_line_length = grid.max {|m| m.length }.length
+      @width = grid.max {|m| m.length }.length
+      @height = gird.length
       # 最大幅より短い行は、文字数が足りない分だけ:emptyを持たせる。
       @map = grid.map do |line|
-        if line.length < longest_line_length
-          line += Array.new(longest_line_length - line.length, :empty)
+        if line.length < @width
+          line += Array.new(@width - line.length, :empty)
         end
         line
       end
