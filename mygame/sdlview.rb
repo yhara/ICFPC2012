1246c00a0c05c62046d3d700f32c574fffe69eef
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-15 19:29:09 +0900

    sdl: 画面を800x600にした

--- mygame/sdlview.rb
+++ mygame/sdlview.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require_relative "../dist/src/lib/lambda_lifter"
 $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib/"
-require 'mygame/boot'
+require 'mygame'
 require 'singleton'
 
 class LambdaLifter
@@ -9,22 +9,10 @@ class LambdaLifter
     include Singleton
 
     def initialize
-      here = File.dirname(__FILE__)
-      @images = {
-        closed_lift: Image.new("#{here}/../visualizer/images/closed-lift.png"),
-        earth: Image.new("#{here}/../visualizer/images/earth.png"),
-        empty: Image.new("#{here}/../visualizer/images/empty.png"),
-        lambda: Image.new("#{here}/../visualizer/images/lambda.png"),
-        open_lift: Image.new("#{here}/../visualizer/images/open-lift.png"),
-        robot: Image.new("#{here}/../visualizer/images/robot.png"),
-        rock: Image.new("#{here}/../visualizer/images/rock.png"),
-        wall: Image.new("#{here}/../visualizer/images/wall.png"),
-      }
-
       @mine = Mine.new(<<-'EOD')
 RL
       EOD
-      @scale = 1
+      @scale = nil
     end
     attr_accessor :mine
     attr_writer :scale
@@ -34,7 +22,22 @@ RL
     end
 
     def run
-      main_loop do
+      MyGame.create_screen 800, 600
+      @scale ||= 1
+
+      here = File.dirname(__FILE__)
+      @images = {
+        closed_lift: MyGame::Image.new("#{here}/../visualizer/images/closed-lift.png"),
+        earth: MyGame::Image.new("#{here}/../visualizer/images/earth.png"),
+        empty: MyGame::Image.new("#{here}/../visualizer/images/empty.png"),
+        lambda: MyGame::Image.new("#{here}/../visualizer/images/lambda.png"),
+        open_lift: MyGame::Image.new("#{here}/../visualizer/images/open-lift.png"),
+        robot: MyGame::Image.new("#{here}/../visualizer/images/robot.png"),
+        rock: MyGame::Image.new("#{here}/../visualizer/images/rock.png"),
+        wall: MyGame::Image.new("#{here}/../visualizer/images/wall.png"),
+      }
+
+      MyGame.main_loop do
         @mine.raw_map.each.with_index do |raw_row, y|
           raw_row.each.with_index do |sym, x|
             img = @images[sym] or raise "unkown: #{sym}"
