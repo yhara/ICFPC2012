f22efbf831a3902ceed29961cfc7089efdfdb398
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-16 12:59:43 +0900

    sdlview: 画像読み込みをリファクタリング

--- mygame/sdlview.rb
+++ mygame/sdlview.rb
@@ -7,6 +7,7 @@ require 'singleton'
 class LambdaLifter
   class SdlVisualizer
     include Singleton
+    include MyGame
 
     def initialize
       @mine = Mine.new(<<-'EOD')
@@ -21,34 +22,29 @@ RL
       MyGame.background_color = col
     end
 
+    IMAGE_FILES = %w(closed_lift earth empty lambda open_lift robot rock wall)
     def run
       screen_w = 800
       screen_h = 600
       MyGame.create_screen screen_w, screen_h
 
-      here = File.dirname(__FILE__)
-      @images = {
-        closed_lift: MyGame::Image.new("#{here}/../visualizer/images/closed-lift.png"),
-        earth: MyGame::Image.new("#{here}/../visualizer/images/earth.png"),
-        empty: MyGame::Image.new("#{here}/../visualizer/images/empty.png"),
-        lambda: MyGame::Image.new("#{here}/../visualizer/images/lambda.png"),
-        open_lift: MyGame::Image.new("#{here}/../visualizer/images/open-lift.png"),
-        robot: MyGame::Image.new("#{here}/../visualizer/images/robot.png"),
-        rock: MyGame::Image.new("#{here}/../visualizer/images/rock.png"),
-        wall: MyGame::Image.new("#{here}/../visualizer/images/wall.png"),
-      }
+      images = Hash[IMAGE_FILES.map{|name|
+        [name.to_sym, Image.new("#{File.dirname __FILE__}/../visualizer/images/#{name}.png")]
+      }]
 
       MyGame.main_loop do
         scale = @scale || [(screen_h.to_f/@mine.height)/47, 1].min
         @mine.raw_map.each.with_index do |raw_row, y|
           raw_row.each.with_index do |sym, x|
-            img = @images[sym] or raise "unkown: #{sym}"
+            img = images[sym] or raise "unkown: #{sym}"
             img.x = x * (47*scale)
             img.y = y * (47*scale)
             img.scale = scale
             img.render
           end
         end
+
+        exit if key_pressed?(Key::Q)
       end
     end
   end
