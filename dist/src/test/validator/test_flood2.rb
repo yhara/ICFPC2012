a4266d14c39e261890ab1c3877e97be7615199cb
Yuya.Nishida <yuya@j96.org>
2012-07-16 15:01:44 +0900

    生成した試験の追加

---
+++ dist/src/test/validator/test_flood2.rb
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+=begin
+generated at 2012-07-16T14:50:58+09:00
+
+#######
+#..***#
+#..\\\#
+#...**#
+#.*.*\#
+LR....#
+#######
+
+Flooding 5
+Waterproof 3
+
+|
+| RRUDRRULURULLLLDDDL
+v
+
+#######
+#..   #
+#  *  #
+# ..  #
+#   **#
+O ****#
+#######
+=end
+
+class LambdaLifter
+  class TestFlood2 < Test::Unit::TestCase
+    should "Validatorと同じ結果になる" do
+      mine = Mine.new(<<EOS)
+#######
+#..***#
+#..\\\#
+#...**#
+#.*.*\#
+LR....#
+#######
+
+Flooding 5
+Waterproof 3
+EOS
+      "RRUDRRULURULLLLDDDL".each_char do |s|
+        mine.step!(s)
+      end
+      assert_equal <<EOS, mine.ascii_map
+#######
+#..   #
+#  *  #
+# ..  #
+#   **#
+O ****#
+#######
+EOS
+      assert_equal 281, mine.score
+    end
+  end
+end
